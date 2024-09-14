502148476b98862647dae555a98bc860a5dfd6fe
Vujitha <123562671+Vujitha@users.noreply.github.com>
2024-09-14 20:49:53 +0530

    Update Runner.java
    Runner Reports edit

--- src/test/java/com/Runner/Runner.java
+++ src/test/java/com/Runner/Runner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class) //Ctrl+Shift+O to remove error of cucumber.class
 @CucumberOptions(features="src/test/resources/Features",glue= {"com.stepDefiniton"},
 monochrome=true,
-plugin= {"html:Reports\\HTMLReports"}
+plugin= {"pretty","html:Reports/HTMLReports","json:Reports/cucumber.json"}
 )
 public class Runner {
 
